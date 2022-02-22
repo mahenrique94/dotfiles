fa07be4007d7265f15e2789d544462502b72a9ac
Matheus Castiglioni <mahenrique94@users.noreply.github.com>
2022-02-21 22:23:19 -0300

    Changing font family from Hyper

--- .hyper.js
+++ .hyper.js
@@ -10,7 +10,7 @@ module.exports = {
     // default font size in pixels for all tabs
     fontSize: 16,
     // font family with optional fallbacks
-    fontFamily: "Fira Code",
+    fontFamily: "FiraCode Nerd Font",
     // default font weight: 'normal' or 'bold'
     fontWeight: "normal",
     // font weight for bold characters: 'normal' or 'bold'
