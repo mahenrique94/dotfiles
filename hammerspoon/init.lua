cbab5f5af6e2ac6bbd98e362b695a1ed05a57245
Matheus Castiglioni <mahenrique94@users.noreply.github.com>
2024-04-08 22:08:48 -0300

    Update init.lua

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -5,26 +5,10 @@ hs.hotkey.bind({"cmd", "alt", "ctrl"}, "down", function()
   if focusedWindow then
     local screenFrame = focusedWindow:screen():frame()
     local newFrame = hs.geometry.rect(
-      screenFrame.x + (screenFrame.w - 1920) / 2,          -- X position to center horizontally
+      screenFrame.x,                                       -- X position to center horizontally
       screenFrame.y + screenFrame.h - windowFrame.h,       -- Y position at the bottom of the screen
       1920,                                                -- Width
-      360 - 25                                             -- Height
-    )
-
-    focusedWindow:setFrame(newFrame)
-  end
-end)
-
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "C", function()
-  local focusedWindow = hs.window.focusedWindow()
-
-  if focusedWindow then
-    local screenFrame = focusedWindow:screen():frame()
-    local newFrame = hs.geometry.rect(
-      screenFrame.x + (screenFrame.w - 1920) / 2, -- X position to center horizontally
-      screenFrame.y,                              -- Y position at the top of the screen
-      1920,                                       -- Width
-      1080                                        -- Height
+      screenFrame.h - 1080                                 -- Height
     )
 
     focusedWindow:setFrame(newFrame)
@@ -37,10 +21,10 @@ hs.hotkey.bind({"cmd", "alt", "ctrl"}, "left", function()
   if focusedWindow then
     local screenFrame = focusedWindow:screen():frame()
     local newFrame = hs.geometry.rect(
-      screenFrame.x,                              -- X top left
-      screenFrame.y,                              -- Y top
-      760,                                        -- Width
-      1440                                        -- Height
+      screenFrame.x,                                       -- X position to center horizontally
+      screenFrame.y,                                       -- Y position at the top of the screen
+      1920,                                                -- Width
+      1080                                                 -- Height
     )
 
     focusedWindow:setFrame(newFrame)
@@ -54,10 +38,10 @@ hs.hotkey.bind({"cmd", "alt", "ctrl"}, "right", function()
   if focusedWindow then
     local screenFrame = focusedWindow:screen():frame()
     local newFrame = hs.geometry.rect(
-      screenFrame.x + screenFrame.w - windowFrame.w,      -- X top right
-      screenFrame.y,                                      -- Y top
-      760,                                                -- Width
-      1440                                                -- Height
+      screenFrame.x + screenFrame.w - windowFrame.w,       -- X top right
+      screenFrame.y,                                       -- Y top
+      screenFrame.w - 1920,                                -- Width
+      screenFrame.h                                        -- Height
     )
 
     focusedWindow:setFrame(newFrame)
