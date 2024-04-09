aae4603cbe44025f5872192054e55edb06ddef66
Matheus Castiglioni <mahenrique94@users.noreply.github.com>
2024-04-08 22:16:35 -0300

    Adding split screen recording bind

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,3 +1,38 @@
+-- Left side in split screen recording
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "1", function()
+  local focusedWindow = hs.window.focusedWindow()
+
+  if focusedWindow then
+    local screenFrame = focusedWindow:screen():frame()
+    local newFrame = hs.geometry.rect(
+      screenFrame.x,                                       -- X position at the top right of the screen
+      screenFrame.y,                                       -- Y position at the top of the screen
+      1920 / 2,                                            -- Width
+      1080                                                 -- Height
+    )
+
+    focusedWindow:setFrame(newFrame)
+  end
+end)
+
+-- Right side in split screen recording
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "2", function()
+  local focusedWindow = hs.window.focusedWindow()
+
+  if focusedWindow then
+    local screenFrame = focusedWindow:screen():frame()
+    local newFrame = hs.geometry.rect(
+      screenFrame.x + (1920 / 2),                          -- X position at the right snap from the left window
+      screenFrame.y,                                       -- Y position at the top of the screen
+      1920 / 2,                                            -- Width
+      1080                                                 -- Height
+    )
+
+    focusedWindow:setFrame(newFrame)
+  end
+end)
+
+-- Left down when recording
 hs.hotkey.bind({"cmd", "alt", "ctrl"}, "down", function()
   local focusedWindow = hs.window.focusedWindow()
   local windowFrame = focusedWindow:frame()
@@ -5,7 +40,7 @@ hs.hotkey.bind({"cmd", "alt", "ctrl"}, "down", function()
   if focusedWindow then
     local screenFrame = focusedWindow:screen():frame()
     local newFrame = hs.geometry.rect(
-      screenFrame.x,                                       -- X position to center horizontally
+      screenFrame.x,                                       -- X position at the top right of the screen
       screenFrame.y + screenFrame.h - windowFrame.h,       -- Y position at the bottom of the screen
       1920,                                                -- Width
       screenFrame.h - 1080                                 -- Height
@@ -15,13 +50,14 @@ hs.hotkey.bind({"cmd", "alt", "ctrl"}, "down", function()
   end
 end)
 
+-- Left up when recording
 hs.hotkey.bind({"cmd", "alt", "ctrl"}, "left", function()
   local focusedWindow = hs.window.focusedWindow()
 
   if focusedWindow then
     local screenFrame = focusedWindow:screen():frame()
     local newFrame = hs.geometry.rect(
-      screenFrame.x,                                       -- X position to center horizontally
+      screenFrame.x,                                       -- X position at the top right of the screen
       screenFrame.y,                                       -- Y position at the top of the screen
       1920,                                                -- Width
       1080                                                 -- Height
@@ -31,6 +67,7 @@ hs.hotkey.bind({"cmd", "alt", "ctrl"}, "left", function()
   end
 end)
 
+-- Right when recording
 hs.hotkey.bind({"cmd", "alt", "ctrl"}, "right", function()
   local focusedWindow = hs.window.focusedWindow()
   local windowFrame = focusedWindow:frame()
@@ -48,6 +85,7 @@ hs.hotkey.bind({"cmd", "alt", "ctrl"}, "right", function()
   end
 end)
 
+-- Reload all binds
 hs.hotkey.bind({"cmd", "alt", "ctrl"}, "R", function()
   hs.reload()
 end)
