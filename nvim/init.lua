2e089f7b898c9849e75238114d551831f91d8c02
Matheus Castiglioni <mahenrique94@users.noreply.github.com>
2025-01-12 16:37:18 -0300

    chore: adding new neovim plugins

--- nvim/init.lua
+++ nvim/init.lua
@@ -836,6 +836,67 @@ require('lazy').setup({
     ---@type ibl.config
     --opts = {},
 	--}
+	
+	{
+		"folke/trouble.nvim",
+		opts = {}, -- for default options, refer to the configuration section for custom setup.
+		cmd = "Trouble",
+		keys = {
+			{
+				"<leader>xx",
+				"<cmd>Trouble diagnostics toggle<cr>",
+				desc = "Diagnostics (Trouble)",
+			},
+			{
+				"<leader>xX",
+				"<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+				desc = "Buffer Diagnostics (Trouble)",
+			},
+			{
+				"<leader>cs",
+				"<cmd>Trouble symbols toggle focus=false<cr>",
+				desc = "Symbols (Trouble)",
+			},
+			{
+				"<leader>cl",
+				"<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+				desc = "LSP Definitions / references / ... (Trouble)",
+			},
+			{
+				"<leader>xL",
+				"<cmd>Trouble loclist toggle<cr>",
+				desc = "Location List (Trouble)",
+			},
+			{
+				"<leader>xQ",
+				"<cmd>Trouble qflist toggle<cr>",
+				desc = "Quickfix List (Trouble)",
+			},
+		},
+	},
+
+	{
+		'stevearc/conform.nvim',
+		opts = {},
+	},
+
+	{
+  "	nvimtools/none-ls.nvim",
+		config = function()
+			local null_ls = require("null-ls")
+
+			-- Define your sources
+			null_ls.setup({
+				sources = {
+					-- Example sources
+					null_ls.builtins.formatting.prettier, -- JavaScript/TypeScript formatter
+					null_ls.builtins.diagnostics.eslint, -- JavaScript/TypeScript linter
+					-- Add more sources as needed
+				},
+			})
+		end,
+		dependencies = { "nvim-lua/plenary.nvim" }, -- Ensure plenary.nvim is installed
+	},
 }, {
   ui = {
     icons = {},
